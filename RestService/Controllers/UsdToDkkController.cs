48f9bdbad98ca974495f577d2828f9406537f57d
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-18 10:41:47 +0100

    Add project files.

---
+++ RestService/Controllers/UsdToDkkController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestService.Models;
+
+namespace RestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsdToDkkController : ControllerBase
+    {
+        [HttpGet]
+        public Amount Get(double value)
+        {
+            Amount result = new();
+            result.Value = value * 6.53;
+            result.Currency = "DKK";
+            return result;
+        }
+    }
+}
---
+++ RestService/Models/Amount.cs
@@ -0,0 +1,9 @@
+﻿namespace RestService.Models
+{
+    public class Amount
+    {
+        public double Value { get; set; }
+
+        public string? Currency { get; set; }
+    }
+}
